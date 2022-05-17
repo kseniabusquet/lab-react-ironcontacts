1dd995b5522ffce6565a21798cc8f6867bd77843
Ksenia Busquet <ksenia.busquet@gmail.com>
2022-05-17 19:43:55 -0300

    Iteration 1 - complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+  return <div className="App">
+    <ContactsList/>
+  </div>;
+}
+
+function ContactsList() {
+
+  const firstFive = [contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <h2>IronContacts</h2>
+
+      <table>
+        <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        </thead>
+
+      {firstFive.map(contact => {
+        return (
+          <tbody key = {contact.id}>
+            <tr>
+              <td><img src = {contact.pictureUrl} alt = "contact"/></td>
+              <td>{contact.name}</td>
+              <td>{Math.round(contact.popularity*100)/100}</td>
+            </tr>
+            </tbody>
+        );
+      })}
+      
+      </table>
     </div>
   );
 }
